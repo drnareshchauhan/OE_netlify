ffc701e2bf079af84139bc98a669c1a6c9ffc814
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-28 20:07:45 +0530

    new deploy

---
+++ dist/precache-manifest.d1f2c2783739b24c45d3df84ba769d93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc560a13368d638eb36be8599004682",
+    "url": "https://oe-frontend.vercel.app/index.html"
+  },
+  {
+    "revision": "2bed9be012fd738b9ee9",
+    "url": "https://oe-frontend.vercel.app/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "18dcfaa13c539cdabfe2",
+    "url": "https://oe-frontend.vercel.app/static/css/main.64a25510.chunk.css"
+  },
+  {
+    "revision": "2bed9be012fd738b9ee9",
+    "url": "https://oe-frontend.vercel.app/static/js/2.c952f1c1.chunk.js"
+  },
+  {
+    "revision": "18dcfaa13c539cdabfe2",
+    "url": "https://oe-frontend.vercel.app/static/js/main.05a3e505.chunk.js"
+  },
+  {
+    "revision": "b8fe34e0fd11dc3e3492",
+    "url": "https://oe-frontend.vercel.app/static/js/runtime~main.1711eb9f.js"
+  },
+  {
+    "revision": "4401e82d47a16230fd580bcbdb7c5d49",
+    "url": "https://oe-frontend.vercel.app/static/media/homepage.4401e82d.jpeg"
+  }
+]);
